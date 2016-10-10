e9ff70c52e6782234da8de3a22e81d9884a1e45e
naoyashiga <naoyashiga0@gmail.com>
2016-10-10 10:21:39 +0900

    tsp,tspを読み込む

--- src/Config.js
+++ src/Config.js
@@ -1,8 +1,12 @@
 import myVs from './shader/vs.vert'
 import myFs from './shader/fs.frag'
+import myVsp from './shader/vsp.vert'
+import myFsp from './shader/fsp.frag'
 
 export default {
   vs: myVs,
   fs: myFs,
+  vsp: myVsp,
+  fsp: myFsp,
   run: true,
 }
